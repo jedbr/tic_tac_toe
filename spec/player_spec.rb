97a56deab1bf554f8664d71833877ede3fefd2f3
jedrzej bryll <jedrzej.bryll@gmail.com>
2016-11-09 00:13:33 +0100

    Add spec for TicTacToe::Player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,12 @@
 require 'tic_tac_toe/player'
+
+describe TicTacToe::Player do
+  subject(:player) { TicTacToe::Player.new("jed", "X") }
+  
+  describe "#initialize" do
+    it "initializes object with name and player mark" do
+      expect(player.name).to eq("jed")
+      expect(player.mark).to eq("X")
+    end
+  end
+end
