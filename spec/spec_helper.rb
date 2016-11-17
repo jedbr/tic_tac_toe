18b02532b901541a0fefacbda510451a087e246e
jedrzej bryll <jedrzej.bryll@gmail.com>
2016-11-17 18:38:14 +0100

    Add spec for tic_tac_toe.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+require 'rspec/collection_matchers'
+
+RSpec.configure do |config|
+
+  original_stderr = $stderr
+  original_stdout = $stdout
+
+  config.before(:all) do
+    # Redirect stderr and stdout
+    $stderr = File.open(File::NULL, "w")
+    $stdout = File.open(File::NULL, "w")
+  end
+  
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
---
+++ spec/tic_tac_toe/tic_tac_toe_spec.rb
@@ -0,0 +1,2 @@
+require 'tic_tac_toe'
+require 'spec_helper'
